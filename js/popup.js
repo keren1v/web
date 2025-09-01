bee715f70b56e32ecacbb861dc110892a5c44e39
keren1v <168234796+keren1v@users.noreply.github.com>
2025-09-01 13:24:58 -0600

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,10 @@
+
+let popup = document.getElementById("pap")
+function openpap( ){
+popup.classList.add( "open-pap");
+}
+function closepap( ){
+popup.classList.remove("open-pap")
+alert("Su formulario fue enviado con exito!")
+}
+              
